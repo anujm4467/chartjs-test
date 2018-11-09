097202889ae0ea5e3d16738a77817e5b828b8f96
anujm4467 <anujm4467@gmail.com>
2018-11-09 23:18:53 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+var React = require('react');
+var Component = React.Component;
+var CanvasJSReact = require('./canvasjs.react');
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+ 
+var dataPoints =[];
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+ 
+	render() {	
+		const options = {
+			theme: "light2",
+			title: {
+				text: "Stock Price of NIFTY 50"
+			},
+			axisY: {
+				title: "Price in USD",
+				prefix: "$",
+				includeZero: false
+			},
+			data: [{
+				type: "line",
+				xValueFormatString: "MMM YYYY",
+				yValueFormatString: "$#,##0.00",
+				dataPoints: dataPoints
+			}]
+		}
+		return (
+		<div>
+			<CanvasJSChart options = {options} 
+				 onRef={ref => this.chart = ref}
+			/>
+			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+		</div>
+		);
+	}
+	
+	componentDidMount(){
+		var chart = this.chart;
+		fetch('https://canvasjs.com/data/gallery/react/nifty-stock-price.json')
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(data) {
+			for (var i = 0; i < data.length; i++) {
+				dataPoints.push({
+					x: new Date(data[i].x),
+					y: data[i].y
+				});
+			}
+			chart.render();
+		});
+	}
 }
-
-export default App;
+ 
+module.exports = App;  
